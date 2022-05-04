58e8c914eb2345410da9278e4e6dd080c0c03794
niecheng <614154335@qq.com>
2022-05-04 16:33:45 +0800

    使用jjwt解析jwt

--- springsecurityoauht2demo/src/main/java/com/cheng/demo/controller/UserController.java
+++ springsecurityoauht2demo/src/main/java/com/cheng/demo/controller/UserController.java
@@ -1,9 +1,13 @@
 package com.cheng.demo.controller;
 
+import io.jsonwebtoken.Jwts;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.nio.charset.StandardCharsets;
 
 @RestController
 @RequestMapping("/user")
@@ -14,8 +18,14 @@ public class UserController {
      * @param authentication
      * @return
      */
-    @GetMapping("getCurrentUser")
-    public Object getCurrentUser(Authentication authentication){
-        return authentication.getPrincipal();
+    @RequestMapping("getCurrentUser")
+    public Object getCurrentUser(Authentication authentication, HttpServletRequest request){
+        String header = request.getHeader("Authorization");
+        String token = header.substring(header.indexOf("Bearer")+7);
+        // return authentication.getPrincipal();
+        return Jwts.parser()
+                .setSigningKey("test_key".getBytes(StandardCharsets.UTF_8))
+                .parseClaimsJws(token)
+                .getBody();
     }
 }
